aa645969083263334d22d05498e21f117b3f6fc3
SvenUZ <svkropi@gmail.com>
2022-07-01 08:59:15 +0200

    - try und except hinzugefügt

--- WakeApp/WakeApp.py
+++ WakeApp/WakeApp.py
@@ -29,31 +29,43 @@ while True:
         except ValueError as e:
             return False
         return True
+           
 
     # Inhalt
     print('Herzlich Willkommen zur WakeApp Terminal App')
     print('--------------')
 
-    # Checken ob Datei existiert
+    # Checken ob Datei existiert und ob die Datei eine valide JSON ist
     if(os.path.isfile('data.json')):
         jsonString = json.dumps('data.json')
         if(is_json(jsonString)):
             load = open('data.json', "r")
             jobj = json.load(load)
-            print('letzte Eingaben: \nAnkunftszeit: ', jobj['Ankunftszeit'], '\nLaenge der Fahrt: ', jobj['Fahrzeit'],'Minuten', '\nZeit am Morgen: ', jobj['Zeit am Morgen'],'Minuten', '\nvorgeschlagene Weckzeit: ', jobj['vorgeschlagener Wecker'], 'Uhr\n')
+            print('letzte Eingaben: \nAnkunftszeit: ', jobj['Ankunftszeit'], 'Uhr', '\nLaenge der Fahrt: ', jobj['Fahrzeit'],'Minuten', '\nZeit am Morgen: ', jobj['Zeit am Morgen'],'Minuten', '\nvorgeschlagene Weckzeit: ', jobj['vorgeschlagener Wecker'], 'Uhr\n')
     else:
         new = open("data.json", "w")
 
     print('Geben Sie die Ankunftszeit ein')
-    hhmm = input()
+    try:
+        hhmm = input()
+    except:
+        print('Die Eingabe ist fehlgeschlagen, bitte erneut probieren')
+        break
 
     # Ueberpruefung von Doppelpunkt
-    if(hhmm.find(':') != -1):
-        hhmm = HHMMtoM(hhmm)
-    else:
-        hhmm = int(hhmm) * 60
-
-    CheckTime(hhmm)
+    try:
+        if(hhmm.find(':') != -1):
+            hhmm = HHMMtoM(hhmm)
+        else:
+            hhmm = int(hhmm) * 60
+    except:
+        print('Der Doppelpunkt-Check ist fehlgeschlagen')
+    
+    try:
+        CheckTime(hhmm)
+    except:
+        print('Das Checken der Uhrzeit ist fehlgeschlagen')
+        break
     print('Wie lange benoetigen Sie fuer die Fahrt?')
     fahrzeit = input()
     
@@ -82,7 +94,8 @@ while True:
     write.write(json2)
     write.close()
     
-    print('Deine vorraussichtliche Aufbruchzeit: ', MtoHHMM(UhrzeitWecker), '\n')
-    print('------------------------------\n\n\n')
+    print('------------------------------')
+    print('Deine vorraussichtliche Aufbruchzeit: ', MtoHHMM(UhrzeitWecker), '\n\n\n')
+
 
     
